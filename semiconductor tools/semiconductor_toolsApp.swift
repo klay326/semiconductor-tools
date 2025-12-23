dbc3d2f91bda6c82edd6fbfd349756621ec28655
Klay Adams <klayaa@me.com>
2025-12-23 12:43:54 -0600

    Initial Commit

---
+++ semiconductor tools/semiconductor_toolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  semiconductor_toolsApp.swift
+//  semiconductor tools
+//
+//  Created by Klay Adams on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct semiconductor_toolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ semiconductor toolsTests/semiconductor_toolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  semiconductor_toolsTests.swift
+//  semiconductor toolsTests
+//
+//  Created by Klay Adams on 12/23/25.
+//
+
+import Testing
+@testable import semiconductor_tools
+
+struct semiconductor_toolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
